73c052bd9d1f28079901306ab1869f3ff6de083e
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-04 18:56:30 +0530

    Intial Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">EcoLife</h1>
+      <p data-ns-test="project-description">
+        A Ecommerce website for Environment friendly products
+      </p>
+      <h1 data-ns-test="project-name">InterviewPrep</h1>
+      <p data-ns-test="project-description">
+        A website which contains all the blogs related to web developement
+      </p>
+    </>
+  );
+};
 
 export default App;
